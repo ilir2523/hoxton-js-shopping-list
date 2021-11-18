338b9f17b482bd11e81343b5fe0994489656e13f
ilir2523 <ilirrukaj25@gmail.com>
2021-11-18 10:07:41 +0100

    .toFixed

--- index.js
+++ index.js
@@ -12,11 +12,11 @@ const userProductPrice = Number(prompt("How much does it cost?"))
 if (userProductPrice < 0 || Number.isNaN(userProductPrice)) {
     console.log("Please enter the right price.")
 } else {
-    shoppingList.push(`${userProductName} costs £${userProductPrice}`)
+    shoppingList.push(`${userProductName} costs £${userProductPrice.toFixed(2)}`)
     for (const item of shoppingList) {
         console.log(item)
     }
     const sumShoppingList = 1.20 + 2.0 + 2.0 + 2 + 2.5 + 5
     const sumShoppingListTotal = userProductPrice + sumShoppingList
-    console.log(`The total of yor shopping list is: £${sumShoppingListTotal}`)
+    console.log(`The total of yor shopping list is: £${sumShoppingListTotal.toFixed(2)}`)
 }
